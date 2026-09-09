fe2d8019dbeaca846c9f3b7f07d6a8f639284697
Shreya Gupta <shreya@mgupta.me>
2026-09-09 21:18:32 +0530

    update product name from "Oversized Black T-Shirt" to "Oversized White T-Shirt"

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
     // T-SHIRTS
     {
       id: 1,
-      name: "Oversized Black T-Shirt",
+      name: "Oversized White T-Shirt",
       price: 499,
       category: "T-Shirts",
       colour: "Black",
